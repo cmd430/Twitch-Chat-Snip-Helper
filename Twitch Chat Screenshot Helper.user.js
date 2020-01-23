d3912f96fb6c9d805facc5b0692226df29432341
Bradley Treweek <cmd430@gmail.com>
2020-01-23 21:12:03 +1300

    censor @mentions

--- Twitch Chat Screenshot Helper.user.js
+++ Twitch Chat Screenshot Helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch Chat Screenshot Helper
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.4.1
 // @description  Click chat msg on twitch, hides name and badges then opens 'Twitch Char Snip Helper' to get snip of chat on clipboard
 // @author       Bred
 // @match        https://*.twitch.tv/*
@@ -92,17 +92,22 @@
             chat = chat.cloneNode(true)
 
             let username = chat.querySelector('.chat-line__username span .chat-author__display-name')
+            let mentions = chat.querySelectorAll('.mention-fragment')
             let icons = chat.querySelector('span:not([class])')
             let existingStyle = chat.getAttribute('style')
 
+            mentions.forEach(mention => {
+                mention.setAttribute('style', 'color: rgb(255, 255, 255); background: rgb(255, 255, 255);')
+            })
+
             username.style.background = username.style.color
             icons.setAttribute('style', `background: ${username.style.color}; padding-top: 15px; height: 0; display: inline-block; overflow: hidden; top: 3px; position: relative;`)
-            chat.setAttribute('style', `position: absolute; top: 0; width: 100%; background: rgb(24, 24, 27); z-index: 1; ${existingStyle}`)
+            chat.setAttribute('style', `position: absolute; top: 0; width: 100%; background: rgb(24, 24, 27); z-index: 100; ${existingStyle}`)
             if (chat.classList.contains('bttv-split-chat-alt-bg')) {
-                chat.setAttribute('style', `position: absolute; top: 0; width: 100%; background: rgb(31, 25, 37); z-index: 1; ${existingStyle}`)
+                chat.setAttribute('style', `position: absolute; top: 0; width: 100%; background: rgb(31, 25, 37); z-index: 100; ${existingStyle}`)
             }
             if (chat.classList.contains('bttv-highlighted')) {
-                chat.setAttribute('style', `position: absolute; top: 0; width: 100%; background: rgba(255, 0, 0, 0.3); z-index: 1; ${existingStyle}`)
+                chat.setAttribute('style', `position: absolute; top: 0; width: 100%; background: rgba(255, 0, 0, 0.3); z-index: 100; ${existingStyle}`)
             }
 
             chat_list.prepend(chat)
