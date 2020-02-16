f3dbd4600c9a99ceeca9cf8aa2b5fe5118a67333
cmd430 <cmd430@gmail.com>
2020-02-17 01:17:32 +1300

    hide mod tools

--- Twitch Chat Screenshot Helper.user.js
+++ Twitch Chat Screenshot Helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch Chat Screenshot Helper
 // @namespace    http://tampermonkey.net/
-// @version      0.5.1
+// @version      0.5.2
 // @description  Click chat msg on twitch, hides name and badges then opens 'Twitch Char Snip Helper' to get snip of chat on clipboard
 // @author       Bred
 // @match        https://*.twitch.tv/*
@@ -22,14 +22,8 @@
 (function() {
     'use strict'
 
-    let censorExempt = [
-        'sweet_anita',
-        'steethebutcher',
-        'brandinio_'
-    ]
-    let mentionCensorExempt = [
-        'sweet_anita'
-    ]
+    let censorExempt = []
+    let mentionCensorExempt = []
     censorExempt = censorExempt.concat(mentionCensorExempt)
 
     /**
@@ -76,6 +70,7 @@
     */
     document.addEventListener('click', el => {
         if (el.ctrlKey) return // press ctrl to not snip
+        if (hasClassInTree(el.target, 'chat-line__username') !== null ) return
 
         let body = document.querySelector('body')
         let chat_list = document.querySelector('.chat-list__list-container').parentElement
@@ -105,12 +100,13 @@
             let username = chat.querySelector('.chat-line__username')
             let mentions = chat.querySelectorAll('.chat-line__message-mention')
             let icons = chat.querySelector('.chat-line__message--badges')
+            let actions = chat.querySelector('.ffz--inline-actions')
             let existingStyle = chat.getAttribute('style')
 
             if (!el.shiftKey) {
                 mentions.forEach(mention => {
                     if (!mentionCensorExempt.includes(mention.textContent.toLowerCase().replace('@', ''))) {
-                        mention.setAttribute('style', 'color: rgb(50, 50, 57); background: rgb(50, 50, 57); border-radius: 0; padding: 0;')
+                        mention.setAttribute('style', 'color: rgb(50, 50, 57) !important; background: rgb(50, 50, 57) !important; border-radius: 0; padding: 0;')
                     }
                 })
 
@@ -120,6 +116,7 @@
                 }
                 chat.setAttribute('style', `position: absolute; top: 0; width: 100%; background: rgb(24, 24, 27) !important; background-image: rgb(24, 24, 27) !important; opacity: 1; z-index: 100; ${existingStyle}`)
             }
+            if (actions) actions.setAttribute('style', 'display: none !important;')
 
             chat.classList.add('ffz-custom-color')
             chat_list.prepend(chat)
